7b2e365064653f4ca8d9fc631d73ed6d54aae13f
irianto <good.pratama@gmail.com>
2024-01-26 18:01:24 +0800

    add login with blank username and correct password

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -34,11 +34,17 @@ test.describe('Login', () => {
         await page.getByRole('button', { name: 'Login' }).click();
         await expect(page.getByRole('alert')).toContainText('Invalid credentials');
     })
-
+    
     test('should failed with blank username and password', async ({ page }) => {
         await page.getByRole('button', { name: 'Login' }).click();
-        await expect(page.getByText('Required').first()).toBeVisible();
         await expect(page.getByText('Required').nth(1)).toBeVisible();
+        await expect(page.getByText('Required').first()).toBeVisible();
+    })
+    
+    test('should failed with blank username and correct password', async ({ page }) => {
+        await page.getByRole('textbox', { name: 'password' }).fill(USERS_PASS[0][1]);
+        await page.getByRole('button', { name: 'Login' }).click();
+        await expect(page.getByText('Required').first()).toBeVisible();
     })
 });
 
