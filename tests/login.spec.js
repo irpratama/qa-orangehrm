484fc9a431a7c8ec3b0fc6ecdaf7f35045a502b0
irianto <good.pratama@gmail.com>
2024-01-26 11:14:26 +0800

    tcl06 add test login with blank username and password

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -20,19 +20,25 @@ test.describe('Login', () => {
         await page.getByRole('button', { name: 'Login' }).click();
         await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
     })
-    
+
     test('should failed with wrong username and correct password', async ({ page }) => {
         await page.getByRole('textbox', { name: 'username' }).fill(USERS_PASS[1][0]);
         await page.getByRole('textbox', { name: 'password' }).fill(USERS_PASS[1][1]);
         await page.getByRole('button', { name: 'Login' }).click();
         await expect(page.getByRole('alert')).toContainText('Invalid credentials');
     })
-    
+
     test('should failed with correct username and wrong password', async ({ page }) => {
         await page.getByRole('textbox', { name: 'username' }).fill(USERS_PASS[2][0]);
         await page.getByRole('textbox', { name: 'password' }).fill(USERS_PASS[2][1]);
         await page.getByRole('button', { name: 'Login' }).click();
         await expect(page.getByRole('alert')).toContainText('Invalid credentials');
+    })
+
+    test('should failed with blank username and password', async ({ page }) => {
+        await page.getByRole('button', { name: 'Login' }).click();
+        await expect(page.getByText('Required').first()).toBeVisible();
+        await expect(page.getByText('Required').nth(1)).toBeVisible();
     })
 });
 
